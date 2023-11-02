9dc35b5ab3ddc8b94b93d82549258ce91b4e20cb
Marif226 <marif226@yandex.ru>
2023-11-02 23:03:08 +0600

    Added database connection in dependencies

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/Marif226/melon
 go 1.21.1
 
 require (
-	github.com/go-chi/chi/v5 v5.0.10 // indirect
-	github.com/google/jsonapi v1.0.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/google/jsonapi v1.0.0 // indirect
 )
--- internal/app/provider.go
+++ internal/app/provider.go
@@ -2,15 +2,18 @@ package app
 
 import (
 	"log"
-
 	"github.com/Marif226/melon/internal/config"
 	"github.com/Marif226/melon/internal/handler"
+	"github.com/Marif226/melon/internal/repository"
 	"github.com/Marif226/melon/internal/service"
+	"github.com/jmoiron/sqlx"
 )
 
 type provider struct {
-	httpConfig config.HTTPConfig
-
+	httpConfig 	config.HTTPConfig
+	pgConfig	config.PGConfig
+	postgres	*sqlx.DB
+	repos		*repository.Repository
 	services	*service.Provider
 	handlers	*handler.Provider
 }
@@ -32,9 +35,43 @@ func (p *provider) HTTPConfig() config.HTTPConfig {
 	return p.httpConfig
 }
 
+func (p *provider) PGConfig() config.PGConfig {
+	if p.pgConfig == nil {
+		cfg, err := config.NewPGConfig()
+		if err != nil {
+			log.Fatalf("failed to get postgres config: %s", err.Error())
+		}
+
+		p.pgConfig = cfg
+	}
+
+	return p.pgConfig
+}
+
+func (p *provider) Postgres() *sqlx.DB {
+	if p.postgres == nil {
+		postgres, err := repository.NewPostgresDB(p.PGConfig())
+		if err != nil {
+			log.Fatalf("failed to get postgres: %s", err.Error())
+		}
+
+		p.postgres = postgres
+	}
+
+	return p.postgres
+}
+
+func (p *provider) Repos() *repository.Repository {
+	if p.repos == nil {
+		p.repos = repository.New(p.Postgres())
+	}
+
+	return p.repos
+}
+
 func (p *provider) Services() *service.Provider {
 	if p.services == nil {
-		p.services = service.NewProvider()
+		p.services = service.NewProvider(p.Repos())
 	}
 
 	return p.services
