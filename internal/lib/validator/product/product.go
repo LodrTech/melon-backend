cc2d92d2ebadd0465b15799660244639fa40c12f
Marif226 <marif226@yandex.ru>
2023-11-04 21:44:12 +0600

    Added id validation

--- internal/lib/validator/product/product.go
+++ internal/lib/validator/product/product.go
@@ -1,6 +1,8 @@
 package product
 
 import (
+	"errors"
+
 	"github.com/Marif226/melon/internal/model"
 	"github.com/go-playground/validator/v10"
 )
@@ -10,6 +12,14 @@ func Create(product *model.Product) error {
 	err := validate.Struct(product)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func ValidateID(id int) error {
+	if id < 0 {
+		return errors.New("invalid ID: cannot be less or equal to 0")
 	}
 
 	return nil
