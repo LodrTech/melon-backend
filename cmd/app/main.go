2f47cabaf28d95959bdf74e95bcb94b28873c997
Marif226 <marif226@yandex.ru>
2023-11-11 21:15:50 +0600

    fix(main): update

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -18,15 +18,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to run app: %s", err.Error())
 	}
-
-	// router := chi.NewRouter()
-
-	// s := service.NewProductService()
-	// h := handler.NewProductHandler(
-	// 	s,
-	// )
-
-	// router.Post("/product", h.Create)
-
-	// http.ListenAndServe(":8000", router)
 }
